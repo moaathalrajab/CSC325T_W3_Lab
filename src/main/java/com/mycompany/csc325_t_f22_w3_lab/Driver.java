2c8ed8454bc2843c827f8a090bd423cedaeab59a
soblab <soblab@Whit209-00.it.farmingdale.edu>
2022-09-15 09:49:52 -0400

    Create new Student object and print

--- src/main/java/com/mycompany/csc325_t_f22_w3_lab/Driver.java
+++ src/main/java/com/mycompany/csc325_t_f22_w3_lab/Driver.java
@@ -4,7 +4,9 @@ package com.mycompany.csc325_t_f22_w3_lab;
 public class Driver {
     
     public static void main(String[] args) {
+        Student std = new Student();
         
+        System.out.println("The student is "+ std.toString());
     }
     
 }
